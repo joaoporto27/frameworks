795386dc6bdb9554d713be48d43634febd14c6d8
LuizGabrielLopes <luiz.gabriellopesc@gmail.com>
2025-08-12 10:27:12 -0300

    feat: add metaframework page

---
+++ src/app/metaframework/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./meta.module.css";
+
+export default function MetaFramework() {
+    return (
+        <div className={styles.container}>
+            <h1>Sobre os Metaframeworks</h1>
+            <p>Um meta-framework é um nível acima: ele combina e/ou estende frameworks existentes para criar um ambiente de desenvolvimento ainda mais produtivo, flexível ou especializado. Ele gerencia ou organiza vários frameworks e ferramentas. Costuma ser usado para facilitar integração, performance e escalabilidade. Costuma ser usado para facilitar integração, performance e escalabilidade.</p>
+        </div>
+    );
+}
