5a48e0a39ef48e6576a9ae71101198413599734d
Leonardo <leonardodeoliveira.tds1.senai@gmail.com>
2025-08-12 11:01:19 -0300

    Feat: create cards Biblioteca

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./page.module.css";
 import Cards from "../components/Cards";
+import CardComponent from "../components/CardBiblioteca";
 
 export default function Home() {
     return (
         <div className={styles.container}>
             <Cards />
+
+            <CardComponent />
         </div>
     );
 }
